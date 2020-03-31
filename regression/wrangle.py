c82b7bdc11b4a2d03d595d8edba08ba7ecb6bb24
Faith Kane <faith.x.kane@gmail.com>
2020-03-31 10:03:16 -0500

    Commit wrangle file from exercise walkthrough

--- regression/wrangle.py
+++ regression/wrangle.py
@@ -1,8 +1,34 @@
-#  Throughout the exercises for Regression in Python lessons, you will use the following example scenario: As a customer analyst, I want to know who has spent the most money with us over their lifetime. I have monthly charges and tenure, so I think I will be able to use those two attributes as features to estimate total_charges. I need to do this within an average of $5.00 per customer.
+import pandas as pd
+import numpy as np
 
-# The first step will be to acquire and prep the data. Do your work for this exercise in a file named wrangle.py.
+from env import host, user, password
 
-# Acquire customer_id, monthly_charges, tenure, and total_charges from telco_churn database for all customers with a 2 year contract.
-# Walk through the steps above using your new dataframe. You may handle the missing values however you feel is appropriate.
-# End with a python file wrangle.py that contains the function, wrangle_telco(), that will acquire the data and return a dataframe cleaned with no missing values.
+# function to get the url
 
+def get_db_url(db_name):
+    return f"mysql+pymysql://{user}:{password}@{host}/{db_name}"
+
+# function that passes my query and my url to return df
+
+def get_data_from_sql():
+    query = """
+    SELECT customer_id, monthly_charges, tenure, total_charges
+    FROM customers
+    WHERE contract_type_id = 3;
+    """
+    df = pd.read_sql(query, get_db_url('telco_churn'))
+    return df
+
+# function that rules them all by acquiring and prepping my df for exploration or modeling
+
+def wrangle_telco():
+    """
+    Queries the telco_churn database
+    Returns a clean df with four columns:
+    customer_id(object), monthly_charges(float), tenure(int), total_charges(float)
+    """
+    df = get_data_from_sql()
+    df.tenure.replace(0, 1, inplace=True)
+    df.total_charges.replace(' ', df.monthly_charges, inplace=True)
+    df.total_charges = df.total_charges.astype(float)
+    return df
